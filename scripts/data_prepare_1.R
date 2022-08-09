e45012af881942f5b216c5916db78cb8ef82f907
figarofuga <figarofuga@gmail.com>
2022-08-09 09:30:25 +0000

    add first scripts

---
+++ scripts/data_prepare_1.R
@@ -0,0 +1,11 @@
+
+# load libraries  ---------------------------------------------------
+library(here)
+library(tidyverse)
+library(MatchIt)
+library(cobalt)
+library(gtsummary)
+
+# load data ---------------------------------------------------
+
+df <- readr::read_csv()
