4ba1908b71200d86b4b5f17e5eeef17c5bec3d20
figarofuga <figarofuga@gmail.com>
2022-08-09 09:35:21 +0000

    first data structures

--- scripts/data_prepare_1.R
+++ scripts/data_prepare_1.R
@@ -8,4 +8,10 @@ library(gtsummary)
 
 # load data ---------------------------------------------------
 
-df <- readr::read_csv()
+dat <- readr::read_csv(here::here("raw_data", "iris.csv"))
+
+
+
+# save data ---------------------------------------------------
+
+base::save(dat, file = here::here("processed_data", "prepared_data.RData"))
